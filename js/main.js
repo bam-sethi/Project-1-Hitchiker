fc5d5d52df35ff6ccaddabb03d43dc29c980a2ca
Ambar Sethi <ambarrrsethi@gmail.com>
2015-08-23 17:17:25 +0100

    ship hides then shows

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ $(document).ready(function(){
   
 
 
-$("#play-button").on('click',function(){
-  var limit = 5;
-  for(var i = 0; i < limit; i ++){ randomPos(); };
-  clickSpaceShip();
+  $("#play-button").one('click',function(){
+    randomPos();
+    clickSpaceShip();
+  });
 
-});
 
 
 var ship1 = '<div id ="ship1"></div>';
@@ -15,32 +14,48 @@ $("#board").append(ship1);
 var position = $('#ship1').position();
 
 ///generates a random position on the board, applies it to ship every 2 secs
+  var count = 0;
 function randomPos(){
-  // for(var i = 0; i < limit; i ++){
+  count++;
+  // console.log(count);
   var newLeft = Math.floor(Math.random() * $('#board').width());
-  console.log(newLeft)
+  // console.log(newLeft)
   var newTop = Math.floor(Math.random() * $('#board').height());
   $("#ship1").css({'left': newLeft, 'top': newTop})
-  setTimeout(randomPos, 2000);
-    // setTimeout(randomPos, 2000);
-}
-});
+  var time = setTimeout(randomPos, 1500);
+    // debugger;
+    ///change count to higher num
+    if (count === 5){
+      console.log('end')
+      clearTimeout(time);
+    }
+};
 
+});//end
 
 
 //if ship is clicked add to score
 //else show ship again
 
-
+var score = 0;
 
 //Click the Space ship and it dissapears
 
 function clickSpaceShip(event){
-
-  var spaceShipDissapear= $('#ship1').on('click', function(){
-    if(spaceShipDissapear = true){
-    console.log(this)
-    $(this).toggle(400);
-    };
-  })  
+  var clickShip = $('#ship1').on('click', function(){
+      console.log(this);
+      // $(this).hide(400);
+    if (clickShip = true) {
+        console.log('clicked');
+        console.log(this);
+        $("#ship1").hide().delay(300).queue(function(n) {
+        $(this).show(); n();
+        });
+    }
+      
+    })
 };
+        // score++;
+        // console.log(score)
+
+
