1c6adfd2ac7a7da699228970a7c87def3b15ca94
Ambar Sethi <ambarrrsethi@gmail.com>
2015-08-23 23:06:34 +0100

    some css updates

---
+++ js/soundfile.js
@@ -0,0 +1,25 @@
+  // console.log('hisound')
+
+  // soundManager.setup({
+  //   url: 'music/',
+  //   flashVersion: 9,
+  //   preferFlash: false,
+  //   onready: function() {
+  //     // martinSpeak();
+      
+  //   }
+  // }); 
+
+// var mySound = soundManager.createSound({
+//  url: 'sounds/ithatesme.mp3'
+// });
+// mySound.play();
+
+
+
+//   function martinSpeak() {
+//      soundManager.url = 'swf/soundmanager2.swf'; // path to movie     
+//      soundManager.onload = function() {  //   
+//     soundManager.createSound({id:'marvin-speak', url:'sounds/ithatesme.mp3'});
+//   }
+// };
