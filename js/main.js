9f2254ea01c43f357539499431f60b64d22adf8e
Ambar Sethi <ambarrrsethi@gmail.com>
2015-08-24 12:51:16 +0100

    ship click working, play/home screen changing

--- js/main.js
+++ js/main.js
@@ -1,42 +1,54 @@
-$(document).ready(function(){
-    startScreen();
-$("#play-button").one('click',function(){
-  randomPos();
-  clickSpaceShip();
-});
-
-function startScreen() {
-  var startGame = '<div id = "start-screen">INSTRUCTIONS:<button class="play" id="play-button">PLAY</button></div>';
-  var start = $('#board').html(startGame);
-
-}
-
-var ship1 = '<div id ="ship1"></div>';
-$("#board").append(ship1);
-var position = $('#ship1').position();
-
 ///generates a random position on the board, applies it to ship every 2 secs
 var count = 0;
+
+$(document).ready(function(){
+  playButtonListener();
+});//end doc ready
+
+function playButtonListener() {
+  $("#play-button").one('click',function(){
+    changeBackgroundThenCreateShip();
+  });
+};
+
+function changeBackgroundThenCreateShip(){
+    $('#start-screen').hide()
+    var gameScreen = '<div class = "game-screen"></div>';
+    $('#board').html(gameScreen);
+
+    // create ship after b/g has been changed
+    createSpaceship();  
+};
+
+function createSpaceship(){
+  var ship1 = '<div id="ship1"></div>';
+  $("#board").append(ship1);
+  // only change position once it's been created
+  randomPos();
+  clickSpaceShip();
+}
+
 function randomPos(){
   count++;
+  // debugger
+
+  var position = $('#ship1').position();
+
   // console.log(count);
   var newLeft = Math.floor(Math.random() * $('#board').width());
   // console.log(newLeft)
   var newTop = Math.floor(Math.random() * $('#board').height());
   $("#ship1").css({'left': newLeft, 'top': newTop})
   var time = setTimeout(randomPos, 1500);
+
     // debugger;
     ///change count to higher num
-    if (count === 4){
-      console.log('end')
-      clearTimeout(time);
-      endScreen();
-
-    }
+  if (count === 4){
+    console.log('end')
+    clearTimeout(time);
+    endScreen();
+  }
 };
-
-});//end
-
 
 function endScreen() {
   var gameOver = '<div id ="game-over">GAME OVER<button class="play" id="play-again">PLAY AGAIN</button></div>'
@@ -52,27 +64,31 @@ function endScreen() {
 //Hide and Show space ship on Click 
 //Count Score
 var score = 0;
+
+
+///find a way to call what is clicked on, not the specific ship
 function clickSpaceShip(event){
-  var clickShip = $('#ship1').on('click', function(){
-      console.log(this);
-      // $(this).hide(400);
-    if (clickShip = true) {
-        console.log('clicked');
-        console.log(this);
-        $("#ship1").hide().delay(300).queue(function(n) {
-        $(this).show(); n();
-        });
-        score++;
-        getScore()
-    }
+  console.log(this);
+//use event.target but will have to define what the target is 
+//like maybe class target?
+//look up handller
+//diff between this and event target
+  $('#ship1').on('click', function(){
+    console.log('spaceship this', this);
+    $("#ship1").hide().delay(300).queue(function(n) {
+      $(this).show(); 
+      n();
+    });
+    score++;
+    getScore()
   })
 };
 
 function getScore(){
-console.log(score)
-var sum = score * 10;
-console.log(sum);
-var map = $('#right-col').html("Your Score: " + sum);
+  console.log(score)
+  var sum = score * 10;
+  console.log(sum);
+  var map = $('#right-col').html("Your Score: " + sum);
 }
 
 
